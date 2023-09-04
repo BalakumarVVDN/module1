9dc4c07b600760d010a4fabd2efe7bcb78f0b627
Balakumar Dharaman <bdharaman@extremenetworks.com>
2023-09-04 10:15:50 +0530

    3

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   Color color = Colors.white;
   List<dynamic> colorlist = [];
   Future<void> _picksinglecolor() async {
